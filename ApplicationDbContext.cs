7bf75b0c40797eb3c656850eb9e6bf75943adb45
ItsSquishy21 <162137207+ItsSquishy21@users.noreply.github.com>
2025-11-21 23:14:09 +0200

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CMCS.Web.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Claim> Claims { get; set; }
+        public DbSet<UploadFile> UploadFiles { get; set; }
+    }
+}
---
+++ DatabaseService.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.SQLite;
+using System.IO;
+
+namespace CMCS_Website.Data
+{
+    public class DatabaseService
+    {
+        private readonly string _connectionString;
+
+        public DatabaseService()
+        {
+            string dbPath = Path.Combine(Directory.GetCurrentDirectory(), "Data", "cmcs.db");
+            _connectionString = $"Data Source={dbPath};Version=3;";
+        }
+
+        public SQLiteConnection GetConnection()
+        {
+            var conn = new SQLiteConnection(_connectionString);
+            conn.Open();
+            return conn;
+        }
+    }
+}
